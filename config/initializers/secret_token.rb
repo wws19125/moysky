aef797d1b8c469479095e8362d628311542c6758
wang <wws19125@126.com>
2013-04-18 10:33:33 +0800

    moysky init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Moysky::Application.config.secret_key_base = 'd90a5495e64d4ef48a5ea2502248753d4671940f6ffd32d4a7eb61e0cf87b5f35e67f00200e1c60f632c3a37cf7f052f743f7460e69d9459ffdf29488ab9b64d'
