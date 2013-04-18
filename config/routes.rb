af213ca9a96cf5cf4cb24930bca50ba97740072c
wang <wws19125@126.com>
2013-04-18 11:08:33 +0800

    搞定首页和bootstrap css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Moysky::Application.routes.draw do
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -46,4 +47,5 @@ Moysky::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root :to=> 'home#index'
 end
