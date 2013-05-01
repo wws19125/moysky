34dfd1c2a03f9671937ad46e38fb9966dadb5a66
wang <wws19125@126.com>
2013-05-01 19:04:43 +0800

    new branch

--- app/assets/javascripts/weibos.js
+++ app/assets/javascripts/weibos.js
@@ -1,4 +1,6 @@
 $(function(){
+	$("#msgbox").delegate("a","click",function(){alert($(this).parent("div:last").attr("id"));});
+	$("a").tooltip({title:'tooltip'});
 	//ajax submit
 	document.forms[0].onsubmit = function(){
 	    if($("#weibo_body").val()=="")return false;
@@ -7,9 +9,10 @@ $(function(){
 		    data:$(this).serialize(),
 		    type:'POST',
 		    success:function(data){
-			$("hr:first").after(data);
+			$("#msgbox").prepend(data);
 			$("div .hero-unit:first").hide(0,function(){$(this).slideDown(1500);});
 			$("#weibo_body").val(null);
+			
 		    },
 		    error:function(){
 			alert("error");
